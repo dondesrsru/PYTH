a864e3678d98db40501168183da4aa7eeaed9078
dondesrsru <83570305+dondesrsru@users.noreply.github.com>
2021-05-04 14:36:59 +0530

    Create primenumber.py

---
+++ primenumber.py
@@ -0,0 +1,14 @@
+num = 11
+flag = False
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
