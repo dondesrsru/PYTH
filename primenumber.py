db9d0e16dc51ea448fe4d045768efe499d5a6024
dondesrsru <83570305+dondesrsru@users.noreply.github.com>
2021-05-09 15:13:48 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,12 +1,9 @@
 num = input('enter a number')
 flag = False
 if int(num) > 1:
-    # check for factors
     for i in range(2, int(num)):
         if (int(num) % i) == 0:
-            # if factor is found, set flag to True
             flag = True
-            # break out of loop
             break
 if flag:
     print(num, "is not a prime number")
