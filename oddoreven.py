b25f51886641bf55769ed8f6295ff8591f7df754
dondesrsru <83570305+dondesrsru@users.noreply.github.com>
2021-05-09 15:12:15 +0530

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,18 @@
+x=input('Enter a number')
+y=input('Enter a second number')
+z=input('Enter a last number')
+x=int(x)
+y=int(y)
+z=int(z)
+if x%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if y%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if z%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
