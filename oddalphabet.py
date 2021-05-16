cb735c3e30f598b09d8eb02c7c8d46e713619a92
dondesrsru <83570305+dondesrsru@users.noreply.github.com>
2021-05-16 12:47:31 +0530

    Update oddalphabet.py

--- oddalphabet.py
+++ oddalphabet.py
@@ -1,6 +1,14 @@
-oddalphabets={'a,c,e,g,i,k,m,o,q,s,u,w,y'}
-string= 'My name is Dhruva.'
-if oddalphabets in string:
-    print('true')
-else: 
-    print('false')
+oddalphabets={'a','c','e','g','i','k','m','o','q','s','u','w','y'}
+allOddAlpha = True
+str = 'A C E'
+str = str.replace(' ', '').lower()
+print(str)
+for c in str:
+    if c not in oddalphabets:
+        allOddAlpha = False
+        break;
+ 
+if allOddAlpha:
+    print ("All odd alphabets")
+else:
+    print ("Not all odd alphabets")
