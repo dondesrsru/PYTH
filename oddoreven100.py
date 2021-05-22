125c280cf54b1c37c7d0adf9cb220543c537a921
dondesrsru <83570305+dondesrsru@users.noreply.github.com>
2021-05-22 14:24:34 +0530

    Create oddoreven100.py

---
+++ oddoreven100.py
@@ -0,0 +1,600 @@
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('Enter a number')
+num2=input('Enter a second number')
+num3=input('Enter a last number')
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+if num1%2==0:
+    print('The number is even')
+else:
+    print('The number is odd ')
+if num2%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+if num3%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+num1=input('enter a number')
+num1=int(num1)
+if num1%2==0:
+    print('the number is even')
+else:
+    print('the number is odd')
